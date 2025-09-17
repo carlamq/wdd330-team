666cdea17cd7ff1daa921bd27523724ab2c30041
CarlaMQ <carlaquintanar1907@gmail.com>
2025-09-17 23:04:24 +0900

    team act 2

--- src/js/product.js
+++ src/js/product.js
@@ -1,20 +1,29 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+//import { setLocalStorage,getParam } from "./utils.mjs";
+//import ProductData from "./ProductData.mjs";
+//
+//const dataSource = new ProductData("tents");
+//
+//function addProductToCart(product) {
+//  let cart = getLocalStorage("so-cart") || [];
+//  cart.push(product);
+//  setLocalStorage("so-cart", cart);
+//}
+//// add to cart button event handler
+//async function addToCartHandler(e) {
+//  const product = await dataSource.findProductById(e.target.dataset.id);
+//  addProductToCart(product);
+//}
+//
+//// add listener to Add to Cart button
+//document
+//  .getElementById("addToCart")
+//  .addEventListener("click", addToCartHandler);
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+const productID = getParam("product");
 
-function addProductToCart(product) {
-  let cart = getLocalStorage("so-cart") || [];
-  cart.push(product);
-  setLocalStorage("so-cart", cart);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const product = new ProductDetails(productID, dataSource);
+product.init();
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -21,3 +21,10 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+// return parameter from URL when requested
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(param);
+  return product;
+}
