42c9ee01cb40166c3b6e35c824df645f02d67645
CarlaMQ <carlaquintanar1907@gmail.com>
2025-09-20 17:35:39 +0900

    W02 Individual Activity: Dynamic Product List

--- src/js/product.js
+++ src/js/product.js
@@ -1,23 +1,3 @@
-//import { setLocalStorage,getParam } from "./utils.mjs";
-//import ProductData from "./ProductData.mjs";
-//
-//const dataSource = new ProductData("tents");
-//
-//function addProductToCart(product) {
-//  let cart = getLocalStorage("so-cart") || [];
-//  cart.push(product);
-//  setLocalStorage("so-cart", cart);
-//}
-//// add to cart button event handler
-//async function addToCartHandler(e) {
-//  const product = await dataSource.findProductById(e.target.dataset.id);
-//  addProductToCart(product);
-//}
-//
-//// add listener to Add to Cart button
-//document
-//  .getElementById("addToCart")
-//  .addEventListener("click", addToCartHandler);
 import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
