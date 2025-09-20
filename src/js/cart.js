50c602f2192c42fc1ebbb6f4a28bea54d9542e70
CarlaMQ <carlaquintanar1907@gmail.com>
2025-09-20 22:52:09 +0900

    Fixed Card Error Trello individual activity

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,26 +1,30 @@
 import { getLocalStorage } from "./utils.mjs";
 
+//Fixed Empty Cart Error Individual activity. by adding a check for empty cart, also added "|| []" to avoid error if cart is empty.
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  const cartItems = getLocalStorage("so-cart") || [];//added "|| []" to avoid error if cart is empty
+  if (cartItems.length === 0) {
+    document.querySelector(".product-list").innerHTML = "<p>Your cart is empty</p>";//put a message if cart is empty
+  } else {
+    const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+    document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  }
 }
-
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+    <a href="#" class="cart-card__image">
+      <img
+        src="${item.Image}"
+        alt="${item.Name}"
+      />
+    </a>
+    <a href="#">
+      <h2 class="card__name">${item.Name}</h2>
+    </a>
+    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+    <p class="cart-card__quantity">qty: 1</p>
+    <p class="cart-card__price">$${item.FinalPrice}</p>
+    </li>`;
 
   return newItem;
 }
