5d427d9190ca8f8c390bd41ee00b3bab00b5820c
CarlaMQ <carlaquintanar1907@gmail.com>
2025-09-27 23:18:51 +0900

    product.js error fixed

--- src/js/product.js
+++ src/js/product.js
@@ -6,34 +6,9 @@ import { loadHeaderFooter } from "./utils.mjs";
 const dataSource = new ProductData("tents");
 const productID = getParam("product");
 
-function addProductToCart(item) {
-  let cart = getLocalStorage("so-cart") || [];
-  
-  // Check if item already exists in cart
-  const existingItem = cart.find(cartItem => cartItem.Id === item.Id);
-  
-  if (existingItem) {
-    // If item exists, increment quantity
-    existingItem.quantity = (existingItem.quantity || 1) + 1;
-  } else {
-    // If item doesn't exist, add it with quantity 1
-    item.quantity = 1;
-    cart.push(item);
-  }
+// Cart functionality is now handled in ProductDetails.mjs
 
-  setLocalStorage("so-cart", cart);
-}
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const SelectProduct = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(SelectProduct);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// Event listener is now handled in ProductDetails.mjs
 
 const product = new ProductDetails(productID, dataSource);
 product.init();
