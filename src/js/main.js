1608d6d1a2049070f0c6e30a2ad8d6a1c20d0049
CarlaMQ <carlaquintanar1907@gmail.com>
2025-10-09 23:36:26 +0900

    W04 Individual Task

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,7 @@ import { loadHeaderFooter } from "./utils.mjs";
 
 // Load header and footer
 loadHeaderFooter();
+
+//for the number in the backpack icon
+import { updateCartCount } from "./utils.mjs";
+updateCartCount();
